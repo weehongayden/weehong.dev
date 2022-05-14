27e03d2f30ec6c5d4fa4269216ea0dd5efac7d26
Wee Hong KOH <weehongayden.developer@hotmail.com>
2022-05-14 18:33:26 +0800

    add Readexpro font

---
+++ pages/_document.tsx
@@ -0,0 +1,25 @@
+import { Head, Html, Main, NextScript } from "next/document";
+
+const Document = () => {
+  return (
+    <Html lang="en">
+      <Head>
+        <link
+          rel="preload"
+          href="/fonts/Readexpro.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+        <meta content="#ffffff" name="theme-color" />
+        <meta content="#ffffff" name="msapplication-TileColor" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+};
+
+export default Document;
