f208f710fafe465528aa5baaae77a07a57cbc67c
Wee Hong KOH <weehongayden.developer@hotmail.com>
2022-05-17 01:13:40 +0800

    add contentlayer

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
-/** @type {import('next').NextConfig} */
+const { withContentlayer } = require("next-contentlayer");
+
 const nextConfig = {
   reactStrictMode: true,
 };
 
-module.exports = nextConfig;
+module.exports = withContentlayer(nextConfig);
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import Container from "components/Container";
 import Layout from "components/Layout";
 import type { NextPage } from "next";
 import Image from "next/image";
@@ -6,30 +7,32 @@ import styles from "styles/Home.module.css";
 const Home: NextPage = () => {
   return (
     <Layout>
-      <div className="grid gap-0 grid-cols-1 sm:gap-5 sm:grid-cols-3">
-        <div className="flex flex-col order-2 col-span-2 sm:order-first">
-          <h1 className="text-center font-bold sm:text-left">Wee Hong KOH</h1>
-          <h2 className="text-lg text-gray-700 dark:text-gray-300 text-center my-1 sm:text-left">
-            Software Engineer at{" "}
-            <span className={`${styles.dbsColor} text-lg text-center`}>
-              DBS Bank
-            </span>
-          </h2>
-          <h3 className="text-2xl my-4 text-center sm:text-left">
-            Enjoy developing web application, automating trivia stuff and
-            reading others' source code
-          </h3>
+      <Container>
+        <div className="grid gap-0 grid-cols-1 sm:gap-5 sm:grid-cols-3">
+          <div className="flex flex-col order-2 col-span-2 sm:order-first">
+            <h1 className="text-center font-bold sm:text-left">Wee Hong KOH</h1>
+            <h2 className="text-lg text-gray-700 dark:text-gray-300 text-center my-1 sm:text-left">
+              Software Engineer at{" "}
+              <span className={`${styles.dbsColor} text-lg text-center`}>
+                DBS Bank
+              </span>
+            </h2>
+            <h3 className="text-2xl my-4 text-center sm:text-left">
+              Enjoy developing web application, automating trivia stuff and
+              reading others&apos; source code
+            </h3>
+          </div>
+          <div className="w-[176px] relative mx-auto mb-8 sm:ml-auto sm:mr-0 sm:my-auto">
+            <Image
+              alt="Wee Hong KOH"
+              height={176}
+              width={176}
+              src="/profile.jpg"
+              className="rounded-full filter grayscale"
+            />
+          </div>
         </div>
-        <div className="w-[176px] relative mx-auto mb-8 sm:ml-auto sm:mr-0 sm:my-auto">
-          <Image
-            alt="Wee Hong KOH"
-            height={176}
-            width={176}
-            src="/profile.jpg"
-            className="rounded-full filter grayscale"
-          />
-        </div>
-      </div>
+      </Container>
     </Layout>
   );
 };
