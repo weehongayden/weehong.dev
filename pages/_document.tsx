45134fe8fa82734569af68d1c3ba7cef83252b2b
Wee Hong KOH <weehongayden.developer@hotmail.com>
2022-05-19 23:47:41 +0800

    add google analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -30,6 +30,22 @@ const Document = () => {
           sizes="16x16"
           type="image/png"
         />
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+          }}
+        />
         <meta content="#ffffff" name="theme-color" />
         <meta content="#ffffff" name="msapplication-TileColor" />
       </Head>
